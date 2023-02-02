c70c36ac128372db68b86ec70c88e6cbf091e053
sebastien michea <smichea@gmail.com>
2023-02-02 12:08:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 [
- {
-  SERVER_ADDRESS:"https://wallet.unikbase.dev",
-  BASIC_AUTH:"appclient:8O6vbFH431W7",
-  KEYCLOAK_URL:"https://wallet.unikbase.dev/auth",
-  UNIVERSAL_LINK:"link.unikbase.dev"
-},
- { 
-  SERVER_ADDRESS:"https://k8s.unikbase.dev",
-  BASIC_AUTH:"appclient:8O6vbFH431W7",
-  KEYCLOAK_URL:"https://k8s.unikbase.dev/auth",
-  UNIVERSAL_LINK:"link.unikbase.dev"
-},
-]
+				{
+					"SERVER_ADDRESS": "https://wallet.unikbase.dev",
+					"BASIC_AUTH": "appclient:8O6vbFH431W7",
+					"KEYCLOAK_URL": "https://wallet.unikbase.dev/auth",
+					"UNIVERSAL_LINK": "link.unikbase.dev"
+				},
+				{
+					"SERVER_ADDRESS": "https://k8s.unikbase.dev",
+					"BASIC_AUTH": "appclient:8O6vbFH431W7",
+					"KEYCLOAK_URL": "https://k8s.unikbase.dev/auth",
+					"UNIVERSAL_LINK": "link.unikbase.dev"
+				}
+			]
